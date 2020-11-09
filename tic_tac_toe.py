d2e0038fdf9136230c8a3cafb5ea70a41f64423b
Ufo420 <pelyheroland22@gmail.com>
2020-11-09 17:08:08 +0100

    Few functions implemented

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,13 +1,31 @@
+import sys
+
+
 def init_board():
     """Returns an empty 3-by-3 board (with .)."""
-    board = []
+    board = [[".", ".", "."], [".", ".", "."], [".", ".", "."]]
+
     return board
 
 
 def get_move(board, player):
-    """Returns the coordinates of a valid move for player on board."""
-    row, col = 0, 0
-    return row, col
+    alphabet_to_index = {'A': 0, 'B': 1, 'C': 2}
+    numbers_to_index = {'1': 0, '2': 1, '3': 2}
+    alphabet_valid = ["C", "B", "A"]
+    numbers_valid = ["1", "2", "3"]
+    while True:
+        move_pos = input("Please enter your move")
+        move_pos = move_pos.upper()
+        row = list(move_pos)[0]
+        col = list(move_pos)[1]
+        if move_pos != "" and len(move_pos) == 2:
+            if move_pos == "QUIT":
+                sys.exit(0)
+            elif board[alphabet_to_index[row]][numbers_to_index[col]] == "." and row in alphabet_valid and col in numbers_valid:
+                return (row, col)
+
+
+print(get_move([[".", ".", "."], [".", "x", "."], [".", ".", "0"]], "0"))
 
 
 def get_ai_move(board, player):
@@ -32,8 +50,13 @@ def is_full(board):
 
 
 def print_board(board):
-    """Prints a 3-by-3 board on the screen with borders."""
-    pass
+    print(f"""
+       1   2   3
+    A  {board[0][0]} | {board[0][1]} | {board[0][2]}
+      ---+---+---
+    B  {board[1][0]} | {board[1][1]} | {board[1][2]}
+      ---+---+---
+    C  {board[2][0]} | {board[2][1]} | {board[2][2]}""")
 
 
 def print_result(winner):
@@ -44,7 +67,8 @@ def print_result(winner):
 def tictactoe_game(mode='HUMAN-HUMAN'):
     board = init_board()
 
-    # use get_move(), mark(), has_won(), is_full(), and print_board() to create game logic
+    # use get_move(), mark(), has_won(), is_full(), and print_board()
+    # to create game logic
     print_board(board)
     row, col = get_move(board, 1)
     mark(board, 1, row, col)
@@ -57,5 +81,5 @@ def main_menu():
     tictactoe_game('HUMAN-HUMAN')
 
 
-if __name__ == '__main__':
-    main_menu()
+# if __name__ == '__main__':
+#   main_menu()
